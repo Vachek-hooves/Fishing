42c79d1ff78320b453de4544f1ee856fc9f646cb
Vachek-hooves <vachek0602@gmail.com>
2024-10-30 18:04:15 +0200

    update welcome page

--- screen/WelcomeScreen.js
+++ screen/WelcomeScreen.js
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Dimensions, ImageBackground } from 'react-native'
+import LottieView from 'lottie-react-native'
+
+const { width, height } = Dimensions.get('window');
 
 const WelcomeScreen = () => {
   return (
-    <View>
+    <ImageBackground style={styles.container} source={require('../assets/fishWelcome.png')}>
+      <LottieView
+        source={require('../assets/lottieJson/fisherman.json')}
+        autoPlay
+        loop
+        style={[styles.animation, styles.fishRight]}
+        resizeMode="cover"
+      />
       <Text>WelcomeScreen</Text>
-    </View>
+    </ImageBackground>
   )
 }
 
 export default WelcomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  animation: {
+    width: width,
+    height: height * 0.5,
+  },
+  fishRight: {
+    // transform: [{ rotate: '180deg' }]
+  }
+})
